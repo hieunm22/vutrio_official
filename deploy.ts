71567855656978bf620bd7e72fb5da1f944e6b3c
vutr <me@vutr.io>
2019-04-02 20:06:19 +0700

    Add docker-compose deployment config

--- deploy.ts
+++ deploy.ts
@@ -1,3 +1,5 @@
+// This script is for auto-deploying non-ssl website
+// SSL auto-setup currently under-development
 import ora from 'ora'
 import chalk from 'chalk'
 import * as open from 'open'
@@ -34,7 +36,6 @@ const setup = new Listr([
   },
   {
     title: 'Update & install NginX, Certbot',
-    skip: () => true,
     task: async ctx => {
       const conn = ctx.conn
       await conn.exec('add-apt-repository ppa:certbot/certbot', [], {
@@ -50,7 +51,7 @@ const setup = new Listr([
     task: async ctx => {
       const conn = ctx.conn
       await conn.execCommand('ufw allow ssh')
-      await conn.execCommand(`ufw allow 'Nginx HTTPS'`)
+      await conn.execCommand(`ufw allow 'Nginx Full'`)
       await conn.exec('ufw enable', [], {
         stream: 'both',
         stdin: 'y',
@@ -69,9 +70,7 @@ const setup = new Listr([
         title: 'Prepare remote dir and copy files',
         task: async () => {
           const conn = ctx.conn
-
           await conn.mkdir('/vutr')
-
           const failed = []
           const success = []
           const status = await conn.putDirectory(__dirname + '/dist', '/vutr', {
@@ -94,22 +93,23 @@ const setup = new Listr([
     ])
   },
   {
-    title: 'Setup NginX & SSL',
+    title: 'Setup NginX & SSL (ssl-setup skipped!)',
     task: ctx => new Listr([
       {
         title: 'Setup NginX config',
         task: async () => {
           const conn = ctx.conn
           const throwOnError = err => { throw new Error(err) }
-          await conn.putFile(__dirname + '/vutr.io.conf', '/etc/nginx/sites-available/vutr.io').then(void 0, throwOnError)
-          await conn.putFile(__dirname + '/nginx.conf', '/etc/nginx/nginx.conf').then(void 0, throwOnError)
+          await conn.putFile(__dirname + '/deploy/vutr.io.conf', '/etc/nginx/sites-available/vutr.io').then(void 0, throwOnError)
           await conn.execCommand('ln -s /etc/nginx/sites-available/vutr.io /etc/nginx/sites-enabled/vutr.io')
           await conn.execCommand('rm -r /etc/nginx/sites-enabled/default')
           await conn.execCommand('systemctl start nginx')
+          await conn.execCommand('nginx -s reload')
         }
       },
       {
         title: 'Install free SSL',
+        skip: () => true,
         task: async () => {
           const conn = ctx.conn
           const execCertbot = 'certbot --nginx --non-interactive --agree-tos --email me@vutr.io --redirect --hsts -d vutr.io -d www.vutr.io'
@@ -121,6 +121,7 @@ const setup = new Listr([
       },
       {
         title: 'Verifying Certbot Renewal Process',
+        skip: () => true,
         task: async () => {
           const result = await ctx.conn.execCommand('certbot renew --dry-run')
           if (result.stderr) { throw new Error(result.stderr) }
@@ -130,12 +131,29 @@ const setup = new Listr([
   },
 ])
 
-setup.run().then(ctx => {
-  spinner.info(chalk.white.bgBlue.bold(' << UFW Firewall Status >> '))
-  info(ctx.firewall)
-  spinner.info(chalk.white.bgBlue.bold(' << Certbot Message >> '))
-  info(ctx.certbot)
-  spinner.info(chalk.white.bgGreen.bold(' DONE '))
-  open('vutr.io')
-  process.exit()
-}).catch(fail)
+const update = new Listr([
+  {
+    title: 'Rolling update...',
+    skip: () => true,
+    task: () => {},
+  }
+])
+
+if (task === 'setup') {
+  setup.run().then(ctx => {
+    spinner.info(chalk.white.bgBlue.bold(' << UFW Firewall Status >> '))
+    info(ctx.firewall)
+    spinner.info(chalk.white.bgGreen.bold(' DONE '))
+    open('vutr.io')
+    ctx.conn.dispose()
+  }).catch(fail)
+}
+
+if (task === 'update') {
+  update.run().then(ctx => {
+    spinner.info(chalk.white.bgBlue.bold(' << Updated Finished >> '))
+    info(ctx.copied_files.join(`\n`))
+    open('vutr.io')
+    ctx.conn.dispose()
+  })
+}
